542edd89c251cf5e680a781ac02b799881b714a3
caizx <caizx@chanjet.com>
2019-06-25 13:43:36 +0800

    修改成可以注册

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -12,6 +12,26 @@ module.exports = {
   },
   module: {
     rules: [
+     {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        // options: {
+        //   babelrc: false,
+        //   presets: ['@babel/preset-env', "@babel/preset-react"],
+        //   plugins: ["@babel/plugin-transform-runtime",
+        //       "@babel/plugin-proposal-export-namespace-from",
+        //   	  "@babel/plugin-proposal-export-default-from",
+        //   	  ["@babel/plugin-proposal-decorators",
+        //       {
+        //         "legacy": true
+        //       }],
+	      //     ["@babel/plugin-proposal-class-properties", { "loose": true }]
+        //     ]
+        // }
+      }
+    },
       {
         test: /\.less/,
         loaders: ["style-loader", "css-loader", "less-loader"]
--- src/viewmodel/index.js
+++ src/viewmodel/index.js
@@ -23,6 +23,9 @@ import {
 import {
   Toolbar
 } from './Toolbar';
+import {
+  t
+} from '../translater';
 import _get from 'lodash/get';
 
 export default class ViewStore {
@@ -60,52 +63,61 @@ export default class ViewStore {
 
   createViewModel(obj) {
     const type = (obj.type || '').toLowerCase();
-    switch (type) {
-      // common
-    case 'toolbar':
-      return Toolbar.create(this, obj);
-
-      //  input
-    case 'text':
-    case 'textbox':
-    case 'input':
-    case 'textarea':
-    case 'number':
-    case 'check':
-    case 'checkbox':
-    case 'switch':
-    case 'date':
-    case 'datetime':
-    case 'month':
-    case 'daterange':
-    case 'week':
-    case 'time':
-    case 'select':
-    case 'listselect':
-    case 'treeselect':
-    case 'refselect':
-    case 'treetableselect':
-    case 'inputtable':
-      return Input.create(this, obj);
-
-      // form
-    case 'form':
-      return Form.create(this, obj);
-    case 'cardform':
-      return CardForm.create(this, obj);
-    case 'listgroup':
-      return ListGroup.create(this, obj);
-
-      // display
-    case 'treetable':
-      return Tree.create(this, obj);
-    case 'table':
-      return Table.create(this, obj);
-    case 'chart':
-      return Chart.create(this, obj);
-
-    default:
+    const ViewItemClass = ViewStore.ViewItemClasses.get(type);
+    if (!ViewItemClass) {
+      console.error('view class not be found!', type);
       return null;
+    }
+    const viewItem = ViewItemClass.create(this, obj);
+    if (!viewItem) {
+      console.error('view item create failed!', type);
+      return null;
+    }
+    return viewItem;
+  }
+
+  static ViewItemClasses = new Map();
+
+  static register(...items) {
+    const registerOne = (type, ViewItemClass) => {
+      if (!type) {
+        console.error('view type not be null!', type);
+        return false;
+      }
+      if (!ViewItemClass) {
+        console.error('view class not be null!', type);
+        return false;
+      }
+      if (typeof ViewItemClass.create !== 'function') {
+        console.error('view class can not be create!');
+        return false;
+      }
+      if (ViewStore.ViewItemClasses.has(type.toLowerCase())) {
+        console.error('view type has registerd!', type.toLowerCase());
+        return false;
+      }
+      ViewStore.ViewItemClasses.set(type.toLowerCase(), ViewItemClass);
+      return true;
+    }
+    if (typeof items[0] === 'string') {
+      return registerOne(...items);
+    } else if (typeof items[0] === 'object') {
+      const keys = Object.keys(items[0]);
+      let hasFaield = false;
+      for (const key of keys) {
+        if (!registerOne(key, items[0][key])) {
+          hasFaield = true;
+        }
+      }
+      return hasFaield;
+    } else {
+      let hasFaield = false;
+      for (const it of items) {
+        if (!registerOne(it.type || it.name, it.viewModel || it.view || it.model)) {
+          hasFaield = true;
+        }
+      }
+      return hasFaield;
     }
   }
 
@@ -118,3 +130,39 @@ export default class ViewStore {
     return viewModel;
   }
 }
+
+ViewStore.register({
+  // common
+  'toolbar': Toolbar,
+  //  input
+  'text': Input,
+  'textbox': Input,
+  'input': Input,
+  'textarea': Input,
+  'number': Input,
+  'check': Input,
+  'checkbox': Input,
+  'switch': Input,
+  'date': Input,
+  'datetime': Input,
+  'month': Input,
+  'daterange': Input,
+  'week': Input,
+  'time': Input,
+  'select': Input,
+  'listselect': Input,
+  'treeselect': Input,
+  'refselect': Input,
+  'treetableselect': Input,
+  'inputtable': Input,
+
+  // form
+  'form': Form,
+  'cardform': CardForm,
+  'listgroup': ListGroup,
+
+  // display
+  'treetable': Tree,
+  'table': Table,
+  'chart': Chart
+});
