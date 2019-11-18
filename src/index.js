243d7cae972a224186ff3dddfbb7c6a8374dbb61
caizx <caizx@chanjet.com>
2019-11-18 10:12:24 +0800

    开发重构

--- src/index.js
+++ src/index.js
@@ -6,12 +6,62 @@ import ListGroup from './components/ListGroup';
 import TreeTable from './components/TreeTable';
 import Chart from './components/Chart';
 
-module.exports = {
-  ViewModel,
+import {
+  Input
+} from './Input';
+import {
   Form,
-  CardForm,
-  Toolbar,
-  Chart,
-  ListGroup,
-  TreeTable,
-}
+  Voucher
+} from './Form';
+import {
+  Tree,
+  Table
+} from './FormList';
+import {
+  Chart
+} from './Chart';
+import {
+  ListGroup
+} from './Option';
+import {
+  Toolbar
+} from './Toolbar';
+
+
+ViewStore.register({
+  // common
+  'toolbar': Toolbar,
+  //  input
+  'text': Input,
+  'textbox': Input,
+  'input': Input,
+  'textarea': Input,
+  'number': Input,
+  'check': Input,
+  'checkbox': Input,
+  'switch': Input,
+  'date': Input,
+  'datetime': Input,
+  'month': Input,
+  'daterange': Input,
+  'week': Input,
+  'time': Input,
+  'select': Input,
+  'listselect': Input,
+  'treeselect': Input,
+  'refselect': Input,
+  'treetableselect': Input,
+  'inputtable': Input,
+  //'editabletable': Table,
+
+  // form
+  'form': Form,
+  'cardform': Voucher,
+  'voucher': Voucher,
+  'listgroup': ListGroup,
+
+  // display
+  'treetable': Tree,
+  'table': Table,
+  'chart': Chart
+});
