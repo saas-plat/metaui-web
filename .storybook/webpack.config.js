4d9a4ec9157ab3dc358680a83b3a9d6a9e6356c3
caizx <caizx@chanjet.com>
2019-12-26 20:38:32 +0800

    测试主题

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -11,30 +11,41 @@ module.exports = {
     extensions: ['.js', '.less', '.json']
   },
   module: {
-    rules: [
-     {
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        // options: {
-        //   babelrc: false,
-        //   presets: ['@babel/preset-env', "@babel/preset-react"],
-        //   plugins: ["@babel/plugin-transform-runtime",
-        //       "@babel/plugin-proposal-export-namespace-from",
-        //   	  "@babel/plugin-proposal-export-default-from",
-        //   	  ["@babel/plugin-proposal-decorators",
-        //       {
-        //         "legacy": true
-        //       }],
-	      //     ["@babel/plugin-proposal-class-properties", { "loose": true }]
-        //     ]
-        // }
-      }
-    },
+    rules: [{
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          // options: {
+          //   babelrc: false,
+          //   presets: ['@babel/preset-env', "@babel/preset-react"],
+          //   plugins: ["@babel/plugin-transform-runtime",
+          //       "@babel/plugin-proposal-export-namespace-from",
+          //   	  "@babel/plugin-proposal-export-default-from",
+          //   	  ["@babel/plugin-proposal-decorators",
+          //       {
+          //         "legacy": true
+          //       }],
+          //     ["@babel/plugin-proposal-class-properties", { "loose": true }]
+          //     ]
+          // }
+        }
+      },
       {
         test: /\.less/,
-        loaders: ["style-loader", "css-loader", "less-loader"]
+        loaders: ["style-loader", "css-loader", {
+          loader: 'less-loader', // compiles Less to CSS
+          options: {
+            modifyVars: {
+              'primary-color': '#1DA57A',
+              'link-color': '#1DA57A',
+              'border-radius-base': '2px',
+              'border-radius-base': '0px',
+              'border-radius-sm': '0px', 
+            },
+            javascriptEnabled: true,
+          }
+        }]
       }
     ]
   }
