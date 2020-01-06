8c2aa1ab0a8d41c20781308b7b1a5aa933157219
caizx <caizx@chanjet.com>
2020-01-06 14:35:58 +0800

    壳编辑增加选择行和编辑组件样式

--- src/components/EditableTable/index.js
+++ src/components/EditableTable/index.js
@@ -11,12 +11,44 @@ import {
 import {
   UIComponent
 } from 'saas-plat-metaui';
+import {
+  Resizable
+} from 'react-resizable';
 import './style';
+
+const ResizeableTitle = props => {
+  const {
+    onResize,
+    width,
+    ...restProps
+  } = props;
+
+  if (!width) {
+    return <th {...restProps} />;
+  }
+
+  return (
+    <Resizable
+      width={width}
+      height={0}
+      onResize={onResize}
+      draggableOpts={{ enableUserSelectHack: false }}
+    >
+      <th {...restProps} />
+    </Resizable>
+  );
+};
 
 @observer
 class EditableCell extends UIComponent {
-   render() {
-    const {value,config,column,rowIndex,columnIndex} = this.props;
+  render() {
+    const {
+      value,
+      config,
+      column,
+      rowIndex,
+      columnIndex
+    } = this.props;
     // 支持行按钮，行按钮一直显示
     return (
       <div className="editable-cell">
@@ -24,7 +56,7 @@ class EditableCell extends UIComponent {
           ? <div className="editable-cell-input-wrapper">
               {this.renderItem(column,{
                   autoFocus: true,
-                  onChange: (value)=>this.context.onEvent(config, 'setCell', value, ()=>config.setCell(rowIndex,columnIndex,value)),
+                  onChange: (value)=>this.context.onEvent(config, 'setCell', value, ()=>config.setCell(rowIndex,columnIndex,{value})),
                   onPressEnter:()=>config.endEdit(),
                   onBlur:()=>config.endEdit()
                 })}
@@ -40,13 +72,38 @@ class EditableCell extends UIComponent {
 @observer
 export default class EditableTable extends UIComponent {
 
-  createColumns = (columns, counter={i:0}) => {
-    return columns.map(it => ({
-      ...it,
-      children: it.children && this.createColumns(it.children, counter.i),
-      render: it.children && it.children.length > 0 ? null : (text, record, index) => (
-        <EditableCell config={this.props.config} row={record} value={record[it.dataIndex]} column={it} rowIndex={index} columnIndex={counter.i++} />)
-    }));
+  components = {
+    header: {
+      cell: ResizeableTitle,
+    },
+  };
+
+  handleResize = index => (e, {
+    size
+  }) => {
+    const config = this.props.config;
+    config.setColumn(index, {
+      width: size.width
+    })
+  };
+
+  createColumns = (columns, counter = {
+    i: 0
+  }) => {
+    return columns.map(it => {
+      const columnIndex = counter.i++;
+      return {
+        ...it,
+        onHeaderCell: column => ({
+          width: column.width,
+          onResize: this.handleResize(columnIndex),
+        }),
+        children: it.children && this.createColumns(it.children, counter.i),
+        render: it.children && it.children.length > 0 ? null : (text, row, rowIndex) => (
+          <EditableCell config={this.props.config} row={row} value={(row[it.dataIndex] || {}).value}
+            column={it} rowIndex={rowIndex} columnIndex={columnIndex} />)
+      }
+    });
   }
 
   render() {
@@ -55,28 +112,34 @@ export default class EditableTable extends UIComponent {
       data,
       columns,
       showCheck,
-      bordered,
+      bordered = true,
       showHeader,
       size,
       title,
+      rowIndex,
       buttons,
     } = config;
     const columnItems = this.createColumns(columns);
-    const rowSelection = showCheck? {
+    const rowSelection = showCheck ? {
       onChange: (selectedRowKeys, selectedRows) => {
-         this.context.onEvent(this.props.config, 'selectRow', {keys:selectedRowKeys, rows:selectedRows},()=> config.selectRows(selectedRowKeys))
+        this.context.onEvent(this.props.config, 'selectRow', {
+          keys: selectedRowKeys,
+          rows: selectedRows
+        }, () => config.selectRows(selectedRowKeys))
       },
       getCheckboxProps: (record, index) => ({
         disabled: config.isRowCheckable(index) // Column configuration not to be checked
         //name: record.name,
       }),
-    }: null;
+    } : null;
     return (
       <div className='editableTable'>
         {buttons?<ToolButtons config={{items:buttons}} />:null}
         <Table
           bordered={bordered}
           showHeader={showHeader}
+          className='table'
+          components={this.components}
           size={size}
           title={title}
           pagination={false}
@@ -91,6 +154,7 @@ export default class EditableTable extends UIComponent {
             }
           })}
           rowSelection={rowSelection}
+          rowClassName={(row, index)=> rowIndex === index?'row-selected':''}
           onRow={(row, index)=>({
                 onClick: () => {
                   this.context.onEvent(this.props.config, 'enterRow', {row, index}, ()=>config.enterRow(index))
